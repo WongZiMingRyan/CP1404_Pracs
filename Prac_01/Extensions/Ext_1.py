1869267404dd2a25ade3eff2458ff107b646615d
WongZiMingRyan <RyanZiMing.Wong@my.jcu.edu.au>
2018-11-26 15:47:55 +0800

    Prac_03 done

--- Prac_01/Extensions/Ext_1.py
+++
@@ -1,8 +0,0 @@
-print("""Electricity bill estimator
-""")
-price_per_kwh = float(input("Enter cents per kWh: "))
-daily_use_kwh = float(input("Enter daily use in kWh: "))
-num_days_bill = float(input("Enter number of billing days: "))
-print("")
-estimated_bill = 0.01 * price_per_kwh * daily_use_kwh * num_days_bill
-print("Estimated bill: {:.2f}".format(estimated_bill))
