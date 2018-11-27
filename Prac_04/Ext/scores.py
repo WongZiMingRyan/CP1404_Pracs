c42bf199de574ff9b9adf040839e4fe03ab8e5e5
WongZiMingRyan <RyanZiMing.Wong@my.jcu.edu.au>
2018-11-27 10:04:43 +0800

    Prac_04 W/ Extensions

---
+++ Prac_04/Ext/scores.py
@@ -0,0 +1,53 @@
+"""
+CP1404/CP5632 Practical
+Debugging exercise: almost-working version of a CSV scores file program.
+The scores.csv file stores scores for each subject for 10 people.
+This code reads the lines into lists, saves the first line as a list of subject codes and
+converts the rest of the lines from a list of strings into a list of numbers,
+which it then prints with the maximum value for that subject.
+Nice. Except, it’s broken! It reads the lists per user not per subject so the results are incorrect.
+Use the debugger to follow what it's doing... then fix it.
+"""
+
+
+def main():
+    """Read and display student scores from scores file."""
+    scores_file = open("scores.csv")
+    scores_data = scores_file.readlines()
+    print(scores_data)
+    subjects = scores_data[0].strip().split(",")
+    score_values = []
+    for score_line in scores_data[1:]:
+        score_strings = score_line.strip().split(",")
+        score_numbers = [int(value) for value in score_strings]
+        score_values.append(score_numbers)
+    scores_file.close()
+    """ 
+    for i in range(len(subjects)):
+        print(subjects[i], "Scores:")
+        for score in score_values[i]:
+            print(score)
+        print("Max:", max(score_values[i]))
+        print()
+    print(score_values)
+    """
+    # My code
+
+    new_score_values = []
+    count = 0
+    for x in range(len(subjects)):
+        holding = []
+        for score in score_values:
+            holding.append(score[count])
+        new_score_values.append(holding)
+        count = count + 1
+
+    for i in range(len(subjects)):
+        print(subjects[i], "Scores:")
+        for score in new_score_values[i]:
+            print(score)
+        print("Max:", max(new_score_values[i]))
+        print()
+    # End of my code
+
+main()
