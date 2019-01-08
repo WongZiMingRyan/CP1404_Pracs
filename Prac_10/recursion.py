477f9235c2e911b707e90122dd0c12f02d633c3b
WongZiMingRyan <RyanZiMing.Wong@my.jcu.edu.au>
2019-01-08 10:34:07 +0800

    Prac 10 Done

--- Prac_10/recursion.py
+++ Prac_10/recursion.py
@@ -12,6 +12,7 @@ def do_it(n):
 
 
 # TODO: 1. write down what you think the output of this will be,
+# infinite loop
 # TODO: 2. use the debugger to step through and see what's actually happening
 print(do_it(5))
 
